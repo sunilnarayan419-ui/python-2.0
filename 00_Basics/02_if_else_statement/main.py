3487c59c677cb5bd3342f3987a11f31801150ec1
[SUNIL MANDLOI] <[sunilnarayan419@gmail.com]>
2026-01-30 01:45:21 +0530

    Added

--- 00_Basics/02_if_else_statement/main.py
+++ 00_Basics/02_if_else_statement/main.py
@@ -1,15 +1,15 @@
 # if else statement 
 
-# """if-else allows Python programs to make decisions 
-# by executing code based on conditions."""
+"""if-else allows Python programs to make decisions 
+by executing code based on conditions."""
 
-# name = "sunil narayan" 
-# s_name = input("Enter your name: ").strip
+name = "sunil narayan" 
+s_name = input("Enter your name: ").strip
 
-# if name != s_name: 
-#     print(f"Please enter a valid name !!") 
-# else: 
-#     print(f"Your name match !!") 
+if name != s_name: 
+    print(f"Please enter a valid name !!") 
+else: 
+    print(f"Your name match !!") 
 
 # Ex 
 
---
+++ 00_Basics/07_for_loops/main.py
@@ -0,0 +1,19 @@
+# for loop  and range also 
+
+# try:
+#     num = int(input("Enter a number: "))
+
+#     for i in range(1, 11):
+#         print(f"{num} x {i} = {num * i}")
+
+# except ValueError:
+#     print("Please enter a valid input!!") 
+    
+    
+# level 
+
+import random 
+
+for i in range(4): 
+    print(random.randint(2 ,8))  
+ 
